957b5ebfc9974341d3c90cd501aea36ad3e2df9d
ahmeditalia <34543951+ahmeditalia@users.noreply.github.com>
2019-06-20 19:57:15 +0200

    update sessions data mapping

--- client/src/store/dataMapping/session.jsx
+++ client/src/store/dataMapping/session.jsx
@@ -10,7 +10,9 @@ export const MY_SESSIONS = "my sessions";
 
 export const SESSION_ID = "id";
 export const SESSION_NAME = "sname";
-export const SESSION_HIDDEN = "hidden";
+export const SESSION_PRIVACY = "privacy";
+export const SESSION_OWNER = "owner";
+export const SESSION_USER_ROLE = "role";
 export const SESSION_DESCRIPTION = "description";
 
 export const SESSION_CONNECTED_USERS = "dataSource";
