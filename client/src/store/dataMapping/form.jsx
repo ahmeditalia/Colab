b70f2fcde880028a6dfe306266d2c88649f7eb8b
MOhamedHDrmi <hdrmim7@gmail.com>
2019-06-22 08:05:58 +0200

    Forms Redux updates

--- client/src/store/dataMapping/form.jsx
+++ client/src/store/dataMapping/form.jsx
@@ -1,6 +1,14 @@
 export const SIGN_IN_FORM = "sign in form";
 export const SIGN_UP_FORM = "sign up form";
 export const SESSION_CREATION_FORM = "session creation form";
+export const TASK_FORM = "task form";
+export const ADD_TASK = "add task";
+export const ADD_CASE = "add case";
+export const CASES = "cases";
+export const TASK_INPUTFIELD = "input";
+export const TASK_OUTPUTFIELD = "output";
+export const TASK_WEIGHTFIELD = "weight";
+export const TASK_HINTSFIELD = "hints";
 
 export const OPEN_FORM = true;
 export const CLOSE_FORM = false;
--- client/src/store/reducers/formReducer.jsx
+++ client/src/store/reducers/formReducer.jsx
@@ -1,9 +1,20 @@
-import {SESSION_CREATION_FORM, SIGN_IN_FORM, SIGN_UP_FORM} from "../dataMapping/form";
+import {
+    ADD_CASE,
+    ADD_TASK,
+    CASES,
+    SESSION_CREATION_FORM,
+    SIGN_IN_FORM,
+    SIGN_UP_FORM,
+    TASK_FORM, TASK_HINTSFIELD,
+    TASK_INPUTFIELD, TASK_OUTPUTFIELD, TASK_WEIGHTFIELD
+} from "../dataMapping/form";
 
 const initState = {
     [SIGN_IN_FORM]: false,
     [SIGN_UP_FORM]: false,
-    [SESSION_CREATION_FORM]: false
+    [SESSION_CREATION_FORM]: false,
+    [TASK_FORM]: false,
+    [CASES]:[]
 };
 
 const formReducer = (state = initState ,action)=>{
@@ -23,7 +34,34 @@ const formReducer = (state = initState ,action)=>{
                 ...state,
                 [SESSION_CREATION_FORM]: action.payload
             };
-
+         case TASK_FORM:
+            return {
+                ...state,
+                [TASK_FORM]: action.payload
+            };
+        case ADD_CASE:
+            state[CASES].push({input:'',output:'',weight:'',hints:''});
+            return state;
+        case TASK_INPUTFIELD:
+                state[CASES][action.payload.index][TASK_INPUTFIELD] = action.payload.value;
+                console.log('input');
+                console.log(state[CASES]);
+            return state;
+        case TASK_OUTPUTFIELD:
+            state[CASES][action.payload.index][TASK_OUTPUTFIELD] = action.payload.value;
+            console.log('output');
+            console.log(state[CASES]);
+            return state;
+        case TASK_HINTSFIELD:
+            state[CASES][action.payload.index][TASK_HINTSFIELD] = action.payload.value;
+            console.log('hints');
+            console.log(state[CASES]);
+            return state;
+        case TASK_WEIGHTFIELD:
+            state[CASES][action.payload.index][TASK_WEIGHTFIELD] = action.payload.value;
+            console.log('weight');
+            console.log(state[CASES]);
+            return state;
         default:
             return state;
     }
